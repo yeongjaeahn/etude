1db83c85dd7f879144c8fde6ae4db9b47e345f21
yeongjae <yeongjae9501@gmail.com>
2022-12-15 02:11:33 +0900

    메인 비주얼 끝

--- js/main.js
+++ js/main.js
@@ -41,10 +41,37 @@ $(function () {
   observer.observe(text4);
   observer.observe(text5);
   observer.observe(text6);
+  // ------------비주얼 슬라이드 텍스트 끝
 
+  // heade 찾기 , 메뉴 아이콘 close 생성,제거 스위치  로직
   $('.menu-btn').click(function () {
-    $('.menu-modal-wrap').toggleClass('on');
+    $('.menu-modal-wrap').fadeToggle();
+    $('.search-wrap').css({ display: 'none' });
+    var src =
+      $('.menu-btn a img').attr('src') === 'img/header/menu.png'
+        ? 'img/header/close.png'
+        : 'img/header/menu.png';
+    $('.menu-btn a img').attr('src', src);
+    // menu 버튼 누르고 바로 search 버튼 눌렀을 시 menu 이미지 원래대로변경
+    if ($('.search-btn a img').attr('src') === 'img/header/close.png') {
+      $('.search-btn a img').attr('src', 'img/header/search.png');
+    }
   });
-});
 
-// ----------------비주얼 슬라이드 끝
+  $('.search-btn').click(function () {
+    $('.search-wrap').fadeToggle();
+    $('.menu-modal-wrap').css({ display: 'none' });
+    var src =
+      $('.search-btn a img').attr('src') === 'img/header/search.png'
+        ? 'img/header/close.png'
+        : 'img/header/search.png';
+    $('.search-btn a img').attr('src', src);
+
+    // search 버튼 누르고 바로 menu 버튼 눌렀을 시 search 이미지 원래대로변경
+    if ($('.menu-btn a img').attr('src') === 'img/header/close.png') {
+      $('.menu-btn a img').attr('src', 'img/header/menu.png');
+    }
+  });
+
+  // heade 찾기 , 메뉴 아이콘 close 생성,제거 스위치  로직 끝 ------------
+});
